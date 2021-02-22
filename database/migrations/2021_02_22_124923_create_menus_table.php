d8ed4ee218ef9d3a669a54e5843ce04a60195f47
hase07180 <yuukisoccer07180@gmail.com>
2021-02-22 14:13:52 +0900

    Menuモデルのカラムの設定
    料理名と料理の画像のカラムを追加

--- database/migrations/2021_02_22_124923_create_menus_table.php
+++ database/migrations/2021_02_22_124923_create_menus_table.php
@@ -15,6 +15,9 @@ class CreateMenusTable extends Migration
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
+            // 料理名、料理の画像
+            $table->string('cooking_name', 100);
+            $table->string('cooking_image');
             $table->timestamps();
         });
     }
