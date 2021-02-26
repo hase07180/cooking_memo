5ccc5e41bb47bf30a539eee876ce9be071751751
hase07180 <yuukisoccer07180@gmail.com>
2021-02-26 23:28:53 +0900

    献立一覧ページの大枠作成

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -2,11 +2,20 @@
 
 @section('content')
 
-<h1>献立一覧</h1>
-@foreach ($menus as $menu)
+<div class="container">
+  <h1>献立一覧</h1>
   <br>
-  <h1>{{ $menu->cooking_name }}</h1>
-  <img width="200px" height="150px" src="{{ $menu->cooking_image }}" alt="{{ $menu->cooking_name }}">
-@endforeach
+  <div class="menu-lists">
+    @foreach ($menus as $menu)
+    <div class="menu">
+      <h2 class="menu-name">料理名：{{ $menu->cooking_name }}</h2>
+      <img class="menu-image" src="{{ $menu->cooking_image }}" alt="{{ $menu->cooking_name }}" width="200px" height="150px">
+    </div>
+    @endforeach
+  </div>
+  <div class="shopping-lists">
+
+  </div>
+</div>
 
 @endsection
