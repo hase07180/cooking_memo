f9d130e4847a36f495cc461942a2c6f7d49ed78e
hase07180 <yuukisoccer07180@gmail.com>
2021-03-01 18:16:18 +0900

    不要なコードを削除

--- resources/views/menu/show.blade.php
+++ resources/views/menu/show.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <form method="GET" action=""> --}}
-  {{-- @csrf --}}
   <h1>{{ $menu->cooking_name }}</h1>
   <img class="menu-image" src="{{ $menu->cooking_image }}" alt="{{ $menu->cooking_name }}">
-{{-- </form> --}}
 @endsection
