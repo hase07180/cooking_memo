cccc02ceff0a6c3f18dfdf5c825519b58845a3fe
hase07180 <yuukisoccer07180@gmail.com>
2021-03-07 15:07:03 +0900

    画像サイズの変更

--- resources/views/menu/show.blade.php
+++ resources/views/menu/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>{{ $menu->cooking_name }}</h1>
-  <img class="menu-image" src="{{'/' . $menu->cooking_image }}" alt="{{ $menu->cooking_name }}">
+<link rel="stylesheet" href="{{ asset('/css/show.css') }}">
+  <div class="container">
+    <h1>{{ $menu->cooking_name }}</h1>
+    <img class="menu-image" src="{{'/' . $menu->cooking_image }}" alt="{{ $menu->cooking_name }}">
+  </div>
 @endsection
