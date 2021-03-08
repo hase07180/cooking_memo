4a97a401f1c68160225d3f6f6aa1807afc373020
hase07180 <yuukisoccer07180@gmail.com>
2021-03-08 16:52:07 +0900

    詳細ページの大枠
    bladeの構成のみを記述
    デザインはまだ修正していない

--- resources/views/menu/show.blade.php
+++ resources/views/menu/show.blade.php
@@ -3,7 +3,19 @@
 @section('content')
 <link rel="stylesheet" href="{{ asset('/css/show.css') }}">
   <div class="container">
-    <h1>{{ $menu->cooking_name }}</h1>
-    <img class="menu-image" src="{{'/' . $menu->cooking_image }}" alt="{{ $menu->cooking_name }}">
+    <h1 class="menu-name" >{{ $menu->cooking_name }}</h1>
+
+    <div class="material-lists">
+      <div class="picture">
+        <img class="menu-image" src="{{'/' . $menu->cooking_image }}" alt="{{ $menu->cooking_name }}">
+      </div>
+      <div class="ingredients">
+      </div>
+    </div>
+
+    <div class="material-btn">
+      <input class="btn btn-secondary" type="submit" value="材料を追加する">
+    </div>
+
   </div>
 @endsection
